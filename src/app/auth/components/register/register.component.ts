f7e50ab407f0bdae93124084209e500db9d71ee2
Vlad Romanov <vladromeur@gmail.com>
2025-01-29 18:02:57 +0500

    Создал модуль для авторизации и прописал его роут

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'gm-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {}
