09f458ae1466d6ca5dd785467030024748459238
Vlad Romanov <vladromeur@gmail.com>
2025-01-31 17:53:51 +0500

    Создал экшн register

---
+++ src/app/auth/store/actions/register.action.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from '@ngrx/store'
+import { ActionTypes } from 'src/app/auth/store/actionTypes'
+
+export const register = createAction(
+  ActionTypes.REGISTER,
+  props<{ username: string; email: string; password: string }>(),
+)
