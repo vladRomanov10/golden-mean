c332f59490a4349ff0a3fbc6b297f951f0451c31
Vlad Romanov <vladromeur@gmail.com>
2025-01-31 19:08:09 +0500

    Добавил настройки для Storedevtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StoreModule } from '@ngrx/store'
 import { AppRoutingModule } from 'src/app/app-routing.module'
 import { AppComponent } from 'src/app/app.component'
 import { AuthModule } from 'src/app/auth/auth.module'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from 'src/environments/environment.prod'
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +16,10 @@ import { AuthModule } from 'src/app/auth/auth.module'
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
