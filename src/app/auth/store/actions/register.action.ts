8f92dffbefdee427e8be8bc37d9f9ea627a0c5d9
Vlad Romanov <vladromeur@gmail.com>
2025-01-31 17:55:56 +0500

    bug fix

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -2,7 +2,7 @@ import { createAction, props } from '@ngrx/store'
 
 import { ActionTypes } from 'src/app/auth/store/actionTypes'
 
-export const register = createAction(
+export const registerAction = createAction(
   ActionTypes.REGISTER,
   props<{ username: string; email: string; password: string }>(),
 )
