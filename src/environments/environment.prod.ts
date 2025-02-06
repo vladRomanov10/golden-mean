476e6c9d214995195d0f21a8cb3a82a2b5a6c56c
Vlad Romanov <vladromeur@gmail.com>
2025-02-06 18:17:45 +0500

    Добавил переменную для доступа к апи

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://real-world-app-39656dff2ddc.herokuapp.com/api',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: false,
+  apiUrl: 'https://real-world-app-39656dff2ddc.herokuapp.com/api',
 }
