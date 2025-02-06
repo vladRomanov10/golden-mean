77156c7c3ec6ce76eb0d8648cb0dbf139bfaafed
Vlad Romanov <vladromeur@gmail.com>
2025-02-06 19:14:54 +0500

    bug fix

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,30 +1,30 @@
-// import { Injectable } from '@angular/core'
-//
-// import { map, Observable } from 'rxjs'
-//
-// import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
-// import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
-// import { AuthResponseInterface } from 'src/app/auth/types/authResponse.interface'
-// import { environment } from 'src/environments/environment'
-//
-// import { HttpClient } from '@angular/common/http'
-//
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class AuthService {
-//   constructor(private http: HttpClient) {}
-//
-//   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
-//     const url: string = environment.apiUrl + '/users'
-//
-//     return this.http
-//       .post<AuthResponseInterface>(url, data)
-//       .pipe(
-//         map(
-//           (response: AuthResponseInterface): CurrentUserInterface =>
-//             response.user,
-//         ),
-//       )
-//   }
-// }
+import { Injectable } from '@angular/core'
+
+import { map, Observable } from 'rxjs'
+
+import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
+import { AuthResponseInterface } from 'src/app/auth/types/authResponse.interface'
+import { environment } from 'src/environments/environment'
+
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
+    const url: string = environment.apiUrl + '/users'
+
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(
+        map(
+          (response: AuthResponseInterface): CurrentUserInterface =>
+            response.user,
+        ),
+      )
+  }
+}
