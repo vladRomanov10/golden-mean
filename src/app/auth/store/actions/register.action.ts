44ae2486d3f4071efb53b2891a51558e91a54317
Vlad Romanov <vladromeur@gmail.com>
2025-02-09 15:15:52 +0500

    Добавил экшены SUCCESS, FAILURE

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -2,8 +2,16 @@ import { createAction, props } from '@ngrx/store'
 
 import { ActionTypes } from 'src/app/auth/store/actionTypes'
 import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<RegisterRequestInterface>(),
+  props<{ request: RegisterRequestInterface }>(),
 )
+
+export const registerSuccessActions = createAction(
+  ActionTypes.REGISTER_SUCCESS,
+  props<{ currentUser: CurrentUserInterface }>(),
+)
+
+export const registerFailureActions = createAction(ActionTypes.REGISTER_FAILURE)
