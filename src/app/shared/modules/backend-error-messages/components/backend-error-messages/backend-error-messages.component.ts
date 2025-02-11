5eb0cd10e4b026e26b86d5fe62adf0991a942b53
Vlad Romanov <vladromeur@gmail.com>
2025-02-11 19:00:56 +0500

    Создал компонент для отображения ошибок с сервера

--- src/app/shared/modules/backend-error-messages/components/backend-error-messages/backend-error-messages.component.ts
+++ src/app/shared/modules/backend-error-messages/components/backend-error-messages/backend-error-messages.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
 @Component({
   selector: 'mc-backend-error-messages',
   templateUrl: './backend-error-messages.component.html',
   styleUrl: './backend-error-messages.component.scss',
 })
-export class BackendErrorMessagesComponent {}
+export class BackendErrorMessagesComponent implements OnInit {
+  @Input('backendErrors') backendErrorsProps!: BackendErrorsInterface
+
+  public errorMessages!: string[]
+
+  ngOnInit(): void {
+    this.errorMessages = Object.keys(this.backendErrorsProps.errors).map(
+      (name: string) => {
+        const messages = this.backendErrorsProps.errors[name]
+
+        return `${name} ${messages}`
+      },
+    )
+  }
+}
