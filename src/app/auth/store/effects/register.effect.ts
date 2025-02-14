f3390359a056db5048a608de817cb8fc561498ff
Vlad Romanov <vladromeur@gmail.com>
2025-02-14 15:44:22 +0500

    Добавил redirect на домашнюю страницу при успешной регистрации

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core'
 import { HttpErrorResponse } from '@angular/common/http'
 
 import { createEffect, Actions, ofType } from '@ngrx/effects'
+
 import {
   registerAction,
   registerFailureActions,
   registerSuccessActions,
 } from 'src/app/auth/store/actions/register.action'
-import { catchError, map, of, switchMap } from 'rxjs'
+
+import { catchError, map, of, switchMap, tap } from 'rxjs'
 
 import { AuthService } from 'src/app/auth/services/auth.service'
 
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
+
+import { Router } from '@angular/router'
 
 @Injectable()
 export class RegisterEffect {
@@ -33,8 +37,20 @@ export class RegisterEffect {
     ),
   )
 
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessActions),
+        tap(() => {
+          this.router.navigateByUrl('/')
+        }),
+      ),
+    { dispatch: false },
+  )
+
   constructor(
     private actions$: Actions,
     private authService: AuthService,
+    private router: Router,
   ) {}
 }
