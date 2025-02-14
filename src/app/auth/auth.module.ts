7b5aadb81b0713a50bfc1fdc176a4762c94a1afd
Vlad Romanov <vladromeur@gmail.com>
2025-02-14 15:27:19 +0500

    Зарегистрировал persistence service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from 'src/app/auth/services/auth.service'
 import { EffectsModule } from '@ngrx/effects'
 import { RegisterEffect } from 'src/app/auth/store/effects/register.effect'
 import { BackendErrorMessagesModule } from 'src/app/shared/modules/backend-error-messages/backendErrorMessages.module'
+import { PersistenceService } from 'src/app/shared/services/persistence.service'
 
 const routes: Routes = [
   {
@@ -28,6 +29,6 @@ const routes: Routes = [
     EffectsModule.forFeature([RegisterEffect]),
     BackendErrorMessagesModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, PersistenceService],
 })
 export class AuthModule {}
