8658d39272ce202e330da778a63b9cceba7d7227
Vlad Romanov <vladromeur@gmail.com>
2025-02-17 18:16:47 +0500

    Создал компонент login на основе register

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+
+import { select, Store } from '@ngrx/store'
+import { registerAction } from 'src/app/auth/store/actions/register.action'
+import { Observable } from 'rxjs'
+import {
+  isSubmittingSelector,
+  validationErrorsSelector,
+} from 'src/app/auth/store/selectors'
+
+import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  public form!: FormGroup
+  isSubmitting$!: Observable<boolean>
+  backendErrors$!: Observable<BackendErrorsInterface | null>
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+    this.initializeValues()
+  }
+
+  onSubmit(): void {
+    const request: RegisterRequestInterface = {
+      user: this.form.value,
+    }
+    this.store.dispatch(registerAction({ request }))
+  }
+
+  private initializeForm(): void {
+    this.form = this.fb.group({
+      email: '',
+      password: '',
+    })
+  }
+
+  private initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    this.backendErrors$ = this.store.pipe(select(validationErrorsSelector))
+  }
+}
