0ed0e6309e93fdbbc5beacdbde93733fa046efa7
Vlad Romanov <vladromeur@gmail.com>
2025-02-17 18:14:27 +0500

    Добавил компонент login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,16 +11,21 @@ import { EffectsModule } from '@ngrx/effects'
 import { RegisterEffect } from 'src/app/auth/store/effects/register.effect'
 import { BackendErrorMessagesModule } from 'src/app/shared/modules/backend-error-messages/backendErrorMessages.module'
 import { PersistenceService } from 'src/app/shared/services/persistence.service'
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ]
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
