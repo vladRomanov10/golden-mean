91225a6b25d728f8783481de6156129546551445
Vlad Romanov <vladromeur@gmail.com>
2025-02-17 18:59:07 +0500

    Добавил тип экшена Login

--- src/app/auth/store/actionTypes.ts
+++ src/app/auth/store/actionTypes.ts
@@ -2,4 +2,5 @@ export enum ActionTypes {
   REGISTER = '[Auth] Register',
   REGISTER_SUCCESS = '[Auth] Register success',
   REGISTER_FAILURE = '[Auth] Register failure',
+  LOGIN = '[Auth] Login',
 }
