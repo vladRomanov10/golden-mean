3ada0a972bd85ca7e1712acc01f35666437c48d3
Vlad Romanov <vladromeur@gmail.com>
2025-02-19 16:07:11 +0500

    Добавил loginSuccess и loginFailure actions

--- src/app/auth/store/actions/login.action.ts
+++ src/app/auth/store/actions/login.action.ts
@@ -1,8 +1,20 @@
 import { createAction, props } from '@ngrx/store'
 import { ActionTypes } from 'src/app/auth/store/actionTypes'
 import { LoginRequestInterface } from 'src/app/auth/types/loginRequest.interface'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
 export const loginAction = createAction(
   ActionTypes.LOGIN,
   props<{ request: LoginRequestInterface }>(),
+)
+
+export const loginSuccessAction = createAction(
+  ActionTypes.LOGIN_SUCCESS,
+  props<{ currentUser: CurrentUserInterface }>(),
+)
+
+export const loginFailureAction = createAction(
+  ActionTypes.LOGIN_FAILURE,
+  props<{ errors: BackendErrorsInterface }>(),
 )
