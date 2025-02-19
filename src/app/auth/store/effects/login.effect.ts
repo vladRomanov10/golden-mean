45661bc44021bfc92c391fbe7e4b830c85dcd1ea
Vlad Romanov <vladromeur@gmail.com>
2025-02-19 16:07:43 +0500

    Добавил эффекты для loginAction

--- src/app/auth/store/effects/login.effect.ts
+++ src/app/auth/store/effects/login.effect.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core'
 import { Actions, createEffect, ofType } from '@ngrx/effects'
-import { loginAction } from 'src/app/auth/store/actions/login.action'
-import { switchMap } from 'rxjs'
+import {
+  loginAction,
+  loginFailureAction,
+  loginSuccessAction,
+} from 'src/app/auth/store/actions/login.action'
+import { catchError, of, switchMap, map, tap } from 'rxjs'
 import { AuthService } from 'src/app/auth/services/auth.service'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
+import { Router } from '@angular/router'
 
 @Injectable()
 export class LoginEffect {
@@ -10,13 +16,34 @@ export class LoginEffect {
     this.actions$.pipe(
       ofType(loginAction),
       switchMap(({ request }) => {
-        return this.authService.register()
+        return this.authService.login(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return loginSuccessAction({ currentUser })
+          }),
+          catchError((errorResponse) => {
+            return of(
+              loginFailureAction({ errors: errorResponse.error.errors }),
+            )
+          }),
+        )
       }),
     ),
+  )
+
+  private redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/')
+        }),
+      ),
+    { dispatch: false },
   )
 
   constructor(
     private actions$: Actions,
     private authService: AuthService,
+    private router: Router,
   ) {}
 }
