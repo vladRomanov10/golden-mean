0dcc37b7c34d53bc5b18fa8ccd03a340e55c9501
Vlad Romanov <vladromeur@gmail.com>
2025-02-20 18:49:32 +0500

    Добавил TopBarModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from 'src/app/auth/auth.module'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { environment } from 'src/environments/environment.prod'
 import { EffectsModule } from '@ngrx/effects'
+import { TopBarModule } from 'src/app/shared/modules/topBar/topBar.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,6 +25,7 @@ import { EffectsModule } from '@ngrx/effects'
       logOnly: environment.production,
     }),
     EffectsModule.forRoot([]),
+    TopBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
