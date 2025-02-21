3e7d6c071f8e77014db2f085625f3d697fd4cc15
Vlad Romanov <vladromeur@gmail.com>
2025-02-21 18:41:47 +0500

    Добавил новые селекторы для компонента topBar

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -2,6 +2,7 @@ import { createFeatureSelector, createSelector } from '@ngrx/store'
 import { AppStateInterface } from 'src/app/shared/types/appState.interface'
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface'
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
 
 export const authFeatureSelector =
   createFeatureSelector<AuthStateInterface>('auth')
@@ -15,4 +16,20 @@ export const validationErrorsSelector = createSelector(
   authFeatureSelector,
   (authState: AuthStateInterface): BackendErrorsInterface | null =>
     authState.validationErrors,
+)
+
+export const isLoggedInSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): boolean | null => authState.isLoggedIn,
+)
+
+export const isAnonymousSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): boolean => authState.isLoggedIn === false,
+)
+
+export const currentUserSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): CurrentUserInterface | null =>
+    authState.currentUser,
 )
