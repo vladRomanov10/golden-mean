b1978a70b9fe385171f75c405aad62d0d9553363
Vlad Romanov <vladromeur@gmail.com>
2025-02-21 18:44:12 +0500

    Добавил переменные, которые используются для отрисовки компонента

--- src/app/shared/modules/topBar/components/top-bar/top-bar.component.ts
+++ src/app/shared/modules/topBar/components/top-bar/top-bar.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
+import { select, Store } from '@ngrx/store'
+import {
+  currentUserSelector,
+  isAnonymousSelector,
+  isLoggedInSelector,
+} from 'src/app/auth/store/selectors'
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
-export class TopBarComponent {}
+export class TopBarComponent implements OnInit {
+  public isLoggedIn$!: Observable<boolean | null>
+  public isAnonymous$!: Observable<boolean>
+  public currentUser$!: Observable<CurrentUserInterface | null>
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector))
+    this.isAnonymous$ = this.store.pipe(select(isAnonymousSelector))
+    this.currentUser$ = this.store.pipe(select(currentUserSelector))
+  }
+}
