d0580145dc3c632eebd642d098d8adb7aa33eacb
Vlad Romanov <vladromeur@gmail.com>
2025-02-25 18:50:22 +0500

    Реализовал метод getCookie

--- src/app/shared/services/persistence.service.ts
+++ src/app/shared/services/persistence.service.ts
@@ -2,5 +2,18 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class PersistenceService {
-  // Здесь будут методы для сохранения куков от бэка после удачного login
+  getCookie(name: string): string | null {
+    const nameEQ: string = name + '='
+    const allCookies = document.cookie.split(';')
+
+    const cookie: string | undefined = allCookies.find((cookie: string) => {
+      const c = cookie.trim()
+      return c.indexOf(nameEQ) === 0
+    })
+
+    if (cookie) {
+      return cookie.substring(nameEQ.length, cookie.length)
+    }
+    return null
+  }
 }
