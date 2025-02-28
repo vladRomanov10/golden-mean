d0a62709e87d1a6e795719a3f4e3b113dbacb3f5
Vlad Romanov <vladromeur@gmail.com>
2025-02-28 17:50:45 +0500

    Удалил метод getcookies

--- src/app/shared/services/persistence.service.ts
+++ src/app/shared/services/persistence.service.ts
@@ -1,19 +1,4 @@
 import { Injectable } from '@angular/core'
 
 @Injectable()
-export class PersistenceService {
-  getCookie(name: string): string | null {
-    const nameEQ: string = name + '='
-    const allCookies = document.cookie.split(';')
-
-    const cookie: string | undefined = allCookies.find((cookie: string) => {
-      const c = cookie.trim()
-      return c.indexOf(nameEQ) === 0
-    })
-
-    if (cookie) {
-      return cookie.substring(nameEQ.length, cookie.length)
-    }
-    return null
-  }
-}
+export class PersistenceService {}
