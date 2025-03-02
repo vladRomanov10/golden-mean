9db3951c2b1bac30fcec6fd8bbb3b202de09735f
Vlad Romanov <vladromeur@gmail.com>
2025-03-02 16:24:42 +0500

    Создал компонент

---
+++ src/app/globalFeed/components/globalFeed/globalFeed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-globalFeed',
+  templateUrl: './globalFeed.component.html',
+  styleUrl: './globalFeed.component.scss',
+})
+export class GlobalFeedComponent {}
