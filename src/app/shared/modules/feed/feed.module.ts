473b54113bd8d882142f3eab81eb9ee3d5410a4f
Vlad Romanov <vladromeur@gmail.com>
2025-03-05 18:34:27 +0500

    Добавил в модуль рельюсер

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common'
 import { FeedComponent } from 'src/app/shared/modules/feed/components/feed/feed.component'
 import { EffectsModule } from '@ngrx/effects'
 import { GetFeedEffect } from 'src/app/shared/modules/feed/store/effects/getFeed.effect'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from 'src/app/shared/modules/feed/store/reducers'
+import { FeedService } from 'src/app/shared/modules/feed/services/feed.service'
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers),
+  ],
   exports: [FeedComponent],
+  providers: [FeedService],
 })
 export class FeedModule {}
