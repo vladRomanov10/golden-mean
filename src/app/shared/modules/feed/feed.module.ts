8816c1a6d63e242203d15f19a468b3006a3fade7
Vlad Romanov <vladromeur@gmail.com>
2025-03-05 17:13:25 +0500

    Импортнул в модуль getFeed эффект

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FeedComponent } from 'src/app/shared/modules/feed/components/feed/feed.component'
+import { EffectsModule } from '@ngrx/effects'
+import { GetFeedEffect } from 'src/app/shared/modules/feed/store/effects/getFeed.effect'
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
   exports: [FeedComponent],
 })
 export class FeedModule {}
