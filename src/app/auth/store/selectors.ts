9e081a7642fb0d404acdb0fc720c024bf2d48780
Vlad Romanov <vladromeur@gmail.com>
2025-03-06 17:24:11 +0500

    bug fix

--- src/app/auth/store/selectors.ts
+++ src/app/auth/store/selectors.ts
@@ -1,5 +1,4 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store'
-import { AppStateInterface } from 'src/app/shared/types/appState.interface'
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface'
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
