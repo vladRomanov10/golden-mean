9fc852d3ff0826604c82a840c15888b68bea1804
Vlad Romanov <vladromeur@gmail.com>
2025-03-06 17:27:39 +0500

    Задиспатчил getFeedAction при инициализации компонента

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { getFeedAction } from 'src/app/shared/modules/feed/store/actions/getFeed.action'
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss',
 })
-export class FeedComponent {}
+export class FeedComponent implements OnInit {
+  @Input('apiUrl') apiUrlProps!: string
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.store.dispatch(getFeedAction({ url: this.apiUrlProps }))
+  }
+}
