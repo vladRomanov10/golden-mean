cbbf9ed45664a05816e693c7f4aeb678b8adabc0
Vlad Romanov <vladromeur@gmail.com>
2025-03-09 12:28:23 +0500

    bug fix

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 import { select, Store } from '@ngrx/store'
 import { registerAction } from 'src/app/auth/store/actions/register.action'
@@ -8,8 +8,6 @@ import {
   isSubmittingSelector,
   validationErrorsSelector,
 } from 'src/app/auth/store/selectors'
-import { AuthService } from 'src/app/auth/services/auth.service'
-import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
 import { RegisterRequestInterface } from 'src/app/auth/types/registerRequest.interface'
 import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
@@ -26,7 +24,6 @@ export class RegisterComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private store: Store,
-    private authService: AuthService,
   ) {}
 
   ngOnInit(): void {
