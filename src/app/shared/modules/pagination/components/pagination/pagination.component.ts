1164f86d1d02dc40a37c55ab2771da2533a597dc
Vlad Romanov <vladromeur@gmail.com>
2025-03-12 12:50:32 +0500

    Добавил расчет колличества ссылок в пагинации

--- src/app/shared/modules/pagination/components/pagination/pagination.component.ts
+++ src/app/shared/modules/pagination/components/pagination/pagination.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
   @Input('total') totalProps!: number
   @Input('limit') limitProps!: number
   @Input('currentPage') currentPageProps!: number
   @Input('url') urlProps!: string
+
+  private pagesCount!: number
+
+  ngOnInit() {
+    this.pagesCount = Math.ceil(this.totalProps / this.limitProps)
+  }
 }
