54dd6b76fa8c42143e5e1868148030618fcf1d6d
Vlad Romanov <vladromeur@gmail.com>
2025-03-14 18:33:48 +0500

    Bug fix

--- src/app/shared/modules/loading/components/loading/loading.component.ts
+++ src/app/shared/modules/loading/components/loading/loading.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-loading',
-  template: '<div>Loading</div>',
+  template: '<div>Loading...</div>',
 })
 export class LoadingComponent {}
