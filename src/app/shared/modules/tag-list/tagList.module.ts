57abc0e02321b40bf310bcdc95d5964926f9502e
Vlad Romanov <vladromeur@gmail.com>
2025-03-17 20:46:59 +0500

    Создаль модуль и компонент tagList

---
+++ src/app/shared/modules/tag-list/tagList.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { TagListComponent } from 'src/app/shared/modules/tag-list/components/tagList/tagList.component'
+
+@NgModule({
+  declarations: [TagListComponent],
+  imports: [CommonModule],
+})
+export class TagListModule {}
