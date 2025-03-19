5179fa4c66e72c762ecdb8f3ab69c2693bad69e9
Vlad Romanov <vladromeur@gmail.com>
2025-03-19 15:48:02 +0500

    Добавил StoreModule

--- src/app/shared/modules/popularTags/popular-tags.module.ts
+++ src/app/shared/modules/popularTags/popular-tags.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PopularTagsComponent } from 'src/app/shared/modules/popularTags/components/popularTags/popularTags.component'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from 'src/app/shared/modules/popularTags/store/reducers'
 
 @NgModule({
   declarations: [PopularTagsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature('popularTags', reducers)],
   exports: [PopularTagsComponent],
 })
 export class PopularTagsModule {}
