e5306408620f02cc03ca1a08a9a196f05181ec60
Vlad Romanov <vladromeur@gmail.com>
2025-03-19 15:09:40 +0500

    Создал редьюсер для popularTags и прописал начальный стейт

---
+++ src/app/shared/modules/popularTags/store/reducers.ts
@@ -0,0 +1,7 @@
+import { PopularTagsStateInterface } from 'src/app/shared/modules/popularTags/types/popularTagsState.interface'
+
+const initialState: PopularTagsStateInterface = {
+  isLoading: false,
+  error: null,
+  data: null,
+}
