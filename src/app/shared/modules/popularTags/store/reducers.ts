e44fff2eb72280e726efc5d32bd659c8ae262a69
Vlad Romanov <vladromeur@gmail.com>
2025-03-19 15:53:23 +0500

    Прописал одно действие на getPopularTags

--- src/app/shared/modules/popularTags/store/reducers.ts
+++ src/app/shared/modules/popularTags/store/reducers.ts
@@ -1,7 +1,24 @@
 import { PopularTagsStateInterface } from 'src/app/shared/modules/popularTags/types/popularTagsState.interface'
+import { Action, createReducer, on } from '@ngrx/store'
+import { getPopularTagsAction } from 'src/app/shared/modules/popularTags/store/actions/getPopularTags.action'
 
 const initialState: PopularTagsStateInterface = {
   isLoading: false,
   error: null,
   data: null,
+}
+
+const popularTagsReducer = createReducer(
+  initialState,
+  on(
+    getPopularTagsAction,
+    (state: PopularTagsStateInterface): PopularTagsStateInterface => ({
+      ...state,
+      isLoading: true,
+    }),
+  ),
+)
+
+export function reducers(state: PopularTagsStateInterface, action: Action) {
+  return popularTagsReducer(state, action)
 }
