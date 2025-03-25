42d14412265683b5585e8db14262c51c04ca98d0
Vlad Romanov <vladromeur@gmail.com>
2025-03-25 21:22:10 +0500

    Добавил переменные и их инициализацию. Добавил запрос на получение popular tags с бекенда

--- src/app/shared/modules/popularTags/components/popularTags/popularTags.component.ts
+++ src/app/shared/modules/popularTags/components/popularTags/popularTags.component.ts
@@ -1,10 +1,40 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { GetPopularTagsResponseInterface } from 'src/app/shared/modules/popularTags/types/getPopularTagsResponse.interface'
+import { select, Store } from '@ngrx/store'
+import {
+  dataSelector,
+  errorSelector,
+  isLoadingSelector,
+} from 'src/app/shared/modules/popularTags/store/selectors'
+import { getPopularTagsAction } from 'src/app/shared/modules/popularTags/store/actions/getPopularTags.action'
 
 @Component({
   selector: 'app-popular-tags',
   templateUrl: './popularTags.component.html',
   styleUrl: './popularTags.component.scss',
 })
-export class PopularTagsComponent {
+export class PopularTagsComponent implements OnInit {
   @Input('tagsApiUrl') tagsApiUrlProps!: string
+
+  public isLoading$!: Observable<boolean>
+  public error$!: Observable<string | null>
+  public data$!: Observable<GetPopularTagsResponseInterface | null>
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.fetchData()
+    this.initializeValues()
+  }
+
+  private fetchData(): void {
+    this.store.dispatch(getPopularTagsAction({ url: this.tagsApiUrlProps }))
+  }
+
+  private initializeValues(): void {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+    this.data$ = this.store.pipe(select(dataSelector))
+  }
 }
