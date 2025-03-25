aac280a770c1beaa6bda3e62ca0e1d77ecc8c90e
Vlad Romanov <vladromeur@gmail.com>
2025-03-25 21:00:21 +0500

    Создал селекторы для popularTags

---
+++ src/app/shared/modules/popularTags/store/selectors.ts
@@ -0,0 +1,22 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store'
+import { PopularTagsStateInterface } from 'src/app/shared/modules/popularTags/types/popularTagsState.interface'
+import { GetPopularTagsResponseInterface } from 'src/app/shared/modules/popularTags/types/getPopularTagsResponse.interface'
+
+export const popularTagsFeatureSelector =
+  createFeatureSelector<PopularTagsStateInterface>('popular tags')
+
+export const isLoadingSelector = createSelector(
+  popularTagsFeatureSelector,
+  (popularTagsState): boolean => popularTagsState.isLoading,
+)
+
+export const errorSelector = createSelector(
+  popularTagsFeatureSelector,
+  (popularTagsState): string | null => popularTagsState.error,
+)
+
+export const dataSelector = createSelector(
+  popularTagsFeatureSelector,
+  (popularTagsStore): GetPopularTagsResponseInterface | null =>
+    popularTagsStore.data,
+)
