2553b20f58e1cdf638f1d3cd8957939028c2debd
Vlad Romanov <vladromeur@gmail.com>
2025-04-09 18:48:13 +0500

    Закончил эффект для getPopularTags

--- src/app/shared/modules/popularTags/store/effects/getPopularTags.effect.ts
+++ src/app/shared/modules/popularTags/store/effects/getPopularTags.effect.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core'
-import { createEffect } from '@ngrx/effects'
-import { Action } from '@ngrx/store'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import {
+  getPopularTagsAction,
+  getPopularTagsFailureAction,
+  getPopularTagsSuccessAction,
+} from 'src/app/shared/modules/popularTags/store/actions/getPopularTags.action'
+import { catchError, map, of, switchMap } from 'rxjs'
+import { GetPopularTagsService } from 'src/app/shared/modules/popularTags/services/getPopularTags.service'
+import { GetPopularTagsResponseInterface } from 'src/app/shared/modules/popularTags/types/getPopularTagsResponse.interface'
 
 @Injectable()
 export class GetPopularTagsEffect {
-  private getPopularTags$ = createEffect(() => )
+  private getPopularTags$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getPopularTagsAction),
+      switchMap(({ url }) => {
+        return this.popularTagsService.getPopularTags(url).pipe(
+          map((popularTags: GetPopularTagsResponseInterface) => {
+            return getPopularTagsSuccessAction({ popularTags })
+          }),
+          catchError(() => {
+            return of(getPopularTagsFailureAction)
+          }),
+        )
+      }),
+    ),
+  )
 
-  constructor(private actions$: Action, ) {
-  }
+  constructor(
+    private actions$: Actions,
+    private popularTagsService: GetPopularTagsService,
+  ) {}
 }
