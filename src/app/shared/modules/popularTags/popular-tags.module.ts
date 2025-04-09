e920b0bd7a9ee196ae20ab67642c207d106f4f43
Vlad Romanov <vladromeur@gmail.com>
2025-04-09 18:47:20 +0500

    bug fix

--- src/app/shared/modules/popularTags/popular-tags.module.ts
+++ src/app/shared/modules/popularTags/popular-tags.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common'
 import { PopularTagsComponent } from 'src/app/shared/modules/popularTags/components/popularTags/popularTags.component'
 import { StoreModule } from '@ngrx/store'
 import { reducers } from 'src/app/shared/modules/popularTags/store/reducers'
+import { EffectsModule } from '@ngrx/effects'
+import { GetPopularTagsEffect } from 'src/app/shared/modules/popularTags/store/effects/getPopularTags.effect'
+import { GetPopularTagsService } from 'src/app/shared/modules/popularTags/services/getPopularTags.service'
 
 @NgModule({
   declarations: [PopularTagsComponent],
-  imports: [CommonModule, StoreModule.forFeature('popularTags', reducers)],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature('popularTags', reducers),
+    EffectsModule.forFeature([GetPopularTagsEffect]),
+  ],
   exports: [PopularTagsComponent],
+  providers: [GetPopularTagsService],
 })
 export class PopularTagsModule {}
