91e308f92f62954c03985fbc8a0b0215031fd26d
Vlad Romanov <vladromeur@gmail.com>
2025-04-10 19:03:37 +0500

    Добавил loading, ErrorMessage, RouterLink

--- src/app/shared/modules/popularTags/popularTags.module.ts
+++ src/app/shared/modules/popularTags/popularTags.module.ts
@@ -6,6 +6,9 @@ import { reducers } from 'src/app/shared/modules/popularTags/store/reducers'
 import { EffectsModule } from '@ngrx/effects'
 import { GetPopularTagsEffect } from 'src/app/shared/modules/popularTags/store/effects/getPopularTags.effect'
 import { PopularTagsService } from 'src/app/shared/modules/popularTags/services/popularTags.service'
+import { LoadingModule } from 'src/app/shared/modules/loading/loading.module'
+import { ErrorMessageModule } from 'src/app/shared/modules/errorMessage/errorMessage.module'
+import { RouterLink } from '@angular/router'
 
 @NgModule({
   declarations: [PopularTagsComponent],
@@ -13,6 +16,9 @@ import { PopularTagsService } from 'src/app/shared/modules/popularTags/services/
     CommonModule,
     StoreModule.forFeature('popular tags', reducers),
     EffectsModule.forFeature([GetPopularTagsEffect]),
+    LoadingModule,
+    ErrorMessageModule,
+    RouterLink,
   ],
   exports: [PopularTagsComponent],
   providers: [PopularTagsService],
