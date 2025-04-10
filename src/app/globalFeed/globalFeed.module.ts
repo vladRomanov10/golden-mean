65f0e0526e0c991cc73fa226a836d04cd9b54ef2
Vlad Romanov <vladromeur@gmail.com>
2025-04-10 16:09:04 +0500

    rename

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -4,7 +4,7 @@ import { GlobalFeedComponent } from 'src/app/globalFeed/components/globalFeed/gl
 import { RouterModule } from '@angular/router'
 import { FeedModule } from 'src/app/shared/modules/feed/feed.module'
 import { BannerModule } from 'src/app/shared/modules/banner/banner.module'
-import { PopularTagsModule } from 'src/app/shared/modules/popularTags/popular-tags.module'
+import { PopularTagsModule } from 'src/app/shared/modules/popularTags/popularTags.module'
 
 const routes = [
   {
