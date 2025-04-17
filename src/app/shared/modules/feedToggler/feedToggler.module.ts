fc723978d98c90a23d0a614509153df5022618f1
Vlad Romanov <vladromeur@gmail.com>
2025-04-17 15:30:35 +0500

    добавил RouterModule

--- src/app/shared/modules/feedToggler/feedToggler.module.ts
+++ src/app/shared/modules/feedToggler/feedToggler.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { FeedTogglerComponent } from './components/feed-toggler/feed-toggler.component'
+import { FeedTogglerComponent } from './components/feedToggler/feedToggler.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [FeedTogglerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [FeedTogglerComponent],
 })
 export class FeedTogglerModule {}
