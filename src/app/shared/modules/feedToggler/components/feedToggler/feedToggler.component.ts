2ad8fb16a50c539f716fe8f11ccec79db32eca73
Vlad Romanov <vladromeur@gmail.com>
2025-04-17 15:32:59 +0500

    Написал логику feedToggler.component

--- src/app/shared/modules/feedToggler/components/feedToggler/feedToggler.component.ts
+++ src/app/shared/modules/feedToggler/components/feedToggler/feedToggler.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { select, Store } from '@ngrx/store'
+import { isLoggedInSelector } from 'src/app/auth/store/selectors'
 
 @Component({
   selector: 'app-feed-toggler',
   templateUrl: './feedToggler.component.html',
 })
-export class FeedTogglerComponent {}
+export class FeedTogglerComponent implements OnInit {
+  @Input('tagName') tagNameProps!: string | null
+
+  public isLoggedIn$!: Observable<boolean | null>
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.initializeValues()
+  }
+
+  private initializeValues(): void {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector))
+  }
+}
