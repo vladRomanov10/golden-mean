1c8b20442301524f716784e79c6052b0009de483
Vlad Romanov <vladromeur@gmail.com>
2025-04-24 19:53:15 +0500

    bug fix

--- src/app/yourFeed/yourFeed.module.ts
+++ src/app/yourFeed/yourFeed.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { GlobalFeedComponent } from 'src/app/globalFeed/components/globalFeed/globalFeed.component'
 import { RouterModule } from '@angular/router'
 import { FeedModule } from 'src/app/shared/modules/feed/feed.module'
 import { BannerModule } from 'src/app/shared/modules/banner/banner.module'
