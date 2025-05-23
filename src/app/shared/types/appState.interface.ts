aeda86449e3dd674d62a0af8b148bb2389849533
Vlad Romanov <vladromeur@gmail.com>
2025-05-23 19:08:22 +0500

    Добавил ArticleState

--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -1,9 +1,11 @@
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface'
 import { FeedStateInterface } from 'src/app/shared/modules/feed/types/feedState.interface'
 import { PopularTagsStateInterface } from 'src/app/shared/modules/popularTags/types/popularTagsState.interface'
+import { ArticleStateInterface } from 'src/app/article/types/articleState.interface'
 
 export interface AppStateInterface {
   auth: AuthStateInterface
   feed: FeedStateInterface
   popularTags: PopularTagsStateInterface
+  article: ArticleStateInterface
 }
