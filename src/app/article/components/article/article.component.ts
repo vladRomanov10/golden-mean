67ad4ac956fd74ec45ea09d38a7dea981469ddd5
Vlad Romanov <vladromeur@gmail.com>
2025-05-25 15:59:45 +0500

    Создал article ts

--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,92 +1,59 @@
-import {
-  Component,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { select, Store } from '@ngrx/store'
-import { getFeedAction } from 'src/app/shared/modules/feed/store/actions/getFeed.action'
+import { ActivatedRoute } from '@angular/router'
+import { getArticleAction } from 'src/app/article/store/actions/getArticle.action'
+import { ArticleInterface } from 'src/app/shared/types/article.interface'
 import { Observable, Subscription } from 'rxjs'
-import { GetFeedResponseInterface } from 'src/app/shared/modules/feed/types/getFeedResponse.interface'
 import {
+  articleSelector,
   errorSelector,
-  feedSelector,
   isLoadingSelector,
-} from 'src/app/shared/modules/feed/store/selectors'
-import { environment } from 'src/environments/environment'
-import { ActivatedRoute, Params, Router } from '@angular/router'
-import { parseUrl, stringify } from 'query-string'
+} from 'src/app/article/store/selectors'
 
 @Component({
-  selector: 'app-feed',
+  selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss',
 })
-export class ArticleComponent implements OnInit, OnDestroy, OnChanges {
-  @Input('apiUrl') apiUrlProps!: string
-
+export class ArticleComponent implements OnInit, OnDestroy {
+  public article!: ArticleInterface | null
   public isLoading$!: Observable<boolean>
   public error$!: Observable<string | null>
-  public feed$!: Observable<GetFeedResponseInterface | null>
-  public limit: number = environment.limit
-  public baseUrl!: string
-  public currentPage!: number
+  public isAuthor$!: Observable<boolean>
 
-  private queryParamsSubscription!: Subscription
+  private slug!: string | null
+  private articleSubscription!: Subscription
 
   constructor(
     private store: Store,
-    private router: Router,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit() {
     this.initializeValues()
+    this.fetchData()
     this.initializeListeners()
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    const isApiUrlChanged: boolean =
-      !changes['apiUrlProps'].firstChange &&
-      changes['apiUrlProps'].currentValue !==
-        changes['apiUrlProps'].previousValue
-
-    if (isApiUrlChanged) {
-      this.fetchFeed()
-    }
-  }
-
   ngOnDestroy() {
-    this.queryParamsSubscription.unsubscribe()
+    this.articleSubscription.unsubscribe()
   }
 
-  private initializeValues(): void {
+  private initializeValues() {
+    this.slug = this.route.snapshot.paramMap.get('slug')
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
     this.error$ = this.store.pipe(select(errorSelector))
-    this.feed$ = this.store.pipe(select(feedSelector))
-    this.baseUrl = this.router.url.split('?')[0]
   }
 
-  private initializeListeners(): void {
-    this.queryParamsSubscription = this.route.queryParams.subscribe(
-      (params: Params) => {
-        this.currentPage = Number(params['page'] || '1')
-        this.fetchFeed()
-      },
-    )
+  private fetchData() {
+    this.store.dispatch(getArticleAction({ slug: `${this.slug}` }))
   }
 
-  private fetchFeed(): void {
-    const offset = this.currentPage * this.limit - this.limit
-    const parsedUrl = parseUrl(this.apiUrlProps)
-    const stringifiedParams = stringify({
-      limit: this.limit,
-      offset,
-      ...parsedUrl.query,
-    })
-    const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
-    this.store.dispatch(getFeedAction({ url: apiUrlWithParams }))
+  private initializeListeners() {
+    this.articleSubscription = this.store
+      .pipe(select(articleSelector))
+      .subscribe((article: ArticleInterface | null) => {
+        this.article = article
+      })
   }
 }
