08caa6e32dd873b59ee09b93dee11476a312d17a
Vlad Romanov <vladromeur@gmail.com>
2025-06-06 18:43:56 +0500

    Создал articleFormModule

--- src/app/shared/modules/articleForm/articleForm.module.ts
+++ src/app/shared/modules/articleForm/articleForm.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { CreateArticleComponent } from 'src/app/createArticle/components/createArticle/createArticle.component'
-import { RouterModule } from '@angular/router'
-
-const routes = [{ path: 'articles/new', component: CreateArticleComponent }]
+import { ArticleFormComponent } from 'src/app/shared/modules/articleForm/components/articleForm/articleForm.component'
 
 @NgModule({
-  declarations: [CreateArticleComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [ArticleFormComponent],
+  imports: [CommonModule],
+  exports: [ArticleFormComponent],
 })
-export class CreateArticleModule {}
+export class ArticleFormModule {}
