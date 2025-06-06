23a1c900f83fbe4b9c1c3ea8bb0ddcdd777dd15c
Vlad Romanov <vladromeur@gmail.com>
2025-06-06 18:45:10 +0500

    Прописал инпуты

--- src/app/shared/modules/articleForm/components/articleForm/articleForm.component.ts
+++ src/app/shared/modules/articleForm/components/articleForm/articleForm.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
 @Component({
-  selector: 'app-create-article',
-  templateUrl: './createArticle.component.html',
+  selector: 'app-article-form',
+  templateUrl: './articleForm.component.html',
 })
-export class CreateArticleComponent {}
+export class ArticleFormComponent {
+  @Input('initialValues') initialValuesProps!: ArticleInputInterface
+  @Input('isSubmitting') isSubmittingProps!: boolean
+  @Input('errors') errorsProps!: BackendErrorsInterface | null
+}
