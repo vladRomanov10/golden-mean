9711463ae9953018c28f6430d63ef3a78b77503d
Vlad Romanov <vladromeur@gmail.com>
2025-06-06 18:43:19 +0500

    Добавил ArticleModule

--- src/app/createArticle/createArticle.module.ts
+++ src/app/createArticle/createArticle.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { CreateArticleComponent } from 'src/app/createArticle/components/createArticle/createArticle.component'
 import { RouterModule } from '@angular/router'
+import { ArticleFormModule } from 'src/app/shared/modules/articleForm/articleForm.module'
 
 const routes = [{ path: 'articles/new', component: CreateArticleComponent }]
 
 @NgModule({
   declarations: [CreateArticleComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), ArticleFormModule],
 })
 export class CreateArticleModule {}
