39657d6499b18af118530fb381563844edfa7b20
Vlad Romanov <vladromeur@gmail.com>
2025-06-18 19:39:30 +0500

    Добавил тип в интерфейс для error

--- src/app/article/types/articleState.interface.ts
+++ src/app/article/types/articleState.interface.ts
@@ -1,7 +1,8 @@
 import { ArticleInterface } from 'src/app/shared/types/article.interface'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
 export interface ArticleStateInterface {
   isLoading: boolean
-  error: string | null
+  error: BackendErrorsInterface | null
   data: ArticleInterface | null
 }
