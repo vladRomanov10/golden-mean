85183b668de5a774ece14e36b57563913778a6ba
Vlad Romanov <vladromeur@gmail.com>
2025-06-18 19:34:47 +0500

    Добавил пропс в getArticleFailure

--- src/app/article/store/actions/getArticle.action.ts
+++ src/app/article/store/actions/getArticle.action.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store'
 import { ActionTypes } from 'src/app/article/store/actionTypes'
 import { ArticleInterface } from 'src/app/shared/types/article.interface'
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface'
 
 export const getArticleAction = createAction(
   ActionTypes.GET_ARTICLE,
@@ -12,4 +13,5 @@ export const getArticleSuccessAction = createAction(
 )
 export const getArticleFailureAction = createAction(
   ActionTypes.GET_ARTICLE_FAILURE,
+  props<{ errors: BackendErrorsInterface }>(),
 )
