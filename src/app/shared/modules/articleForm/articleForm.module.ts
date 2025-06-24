87f352ca69a0599a672f9a17bd29f4db29c05f87
Vlad Romanov <vladromeur@gmail.com>
2025-06-24 15:38:45 +0500

    BackendErrorMessagesModule

--- src/app/shared/modules/articleForm/articleForm.module.ts
+++ src/app/shared/modules/articleForm/articleForm.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ArticleFormComponent } from 'src/app/shared/modules/articleForm/components/articleForm/articleForm.component'
 import { ReactiveFormsModule } from '@angular/forms'
+import { BackendErrorMessagesModule } from 'src/app/shared/modules/backendErrorMessages/backendErrorMessages.module'
 
 @NgModule({
   declarations: [ArticleFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, BackendErrorMessagesModule],
   exports: [ArticleFormComponent],
 })
 export class ArticleFormModule {}
