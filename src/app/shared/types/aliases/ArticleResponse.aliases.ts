e9eb8094205efcf367fcf31acfcbdee854f6d9dc
Vlad Romanov <vladromeur@gmail.com>
2025-06-25 17:40:16 +0500

    Создал aliasы для articleInterface

--- src/app/shared/types/aliases/ArticleResponse.aliases.ts
+++ src/app/shared/types/aliases/ArticleResponse.aliases.ts
@@ -1,3 +1,5 @@
 import { ArticleResponseInterface } from 'src/app/shared/types/interfaces/articleResponse.interface'
 
 export type GetArticleResponseInterface = ArticleResponseInterface
+export type PutArticleResponseInterface = ArticleResponseInterface
+export type PostArticleResponseInterface = ArticleResponseInterface
