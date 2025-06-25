3ea45627e0b61bb45486d394d22941a235ddff23
Vlad Romanov <vladromeur@gmail.com>
2025-06-25 17:45:22 +0500

    Прописал логику-заглушку для createArticle

--- src/app/createArticle/components/createArticle/createArticle.component.ts
+++ src/app/createArticle/components/createArticle/createArticle.component.ts
@@ -4,4 +4,15 @@ import { Component } from '@angular/core'
   selector: 'app-create-article',
   templateUrl: './createArticle.component.html',
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  public initialValues = {
+    title: 'Foo',
+    description: 'off',
+    body: 'ahh',
+    tagList: ['join', 'us'],
+  }
+
+  onSubmit(res: any): void {
+    console.log('onSubmit in parent', res)
+  }
+}
