f9e7835c19500eb0e2c5f46de9353c5c03dfb929
Vlad Romanov <vladromeur@gmail.com>
2025-06-25 16:31:18 +0500

    Создал сервис для createArticle

---
+++ src/app/createArticle/services/createArticle.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { ArticleInputInterface } from 'src/app/shared/types/interfaces/articleInput.interface'
+import { map, Observable } from 'rxjs'
+import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
+import { environment } from 'src/environments/environment'
+import { PostArticleResponseInterface } from 'src/app/shared/types/aliases/ArticleResponse.aliases'
+
+@Injectable()
+export class CreateArticleService {
+  constructor(private http: HttpClient) {}
+
+  createArticle(
+    articleInput: ArticleInputInterface,
+  ): Observable<ArticleInterface> {
+    const fullUrl = environment.apiUrl + '/articles'
+
+    return this.http
+      .post<PostArticleResponseInterface>(fullUrl, articleInput)
+      .pipe(map((response: PostArticleResponseInterface) => response.article))
+  }
+}
