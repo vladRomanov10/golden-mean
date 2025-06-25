56ee822d2a4c4db1b707a2ffba8fbf29fbd7b6f2
Vlad Romanov <vladromeur@gmail.com>
2025-06-25 18:11:03 +0500

    Создал заглушку для эффектов createArticle

---
+++ src/app/createArticle/store/effects/createArticle.effect.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import {
+  loginAction,
+  loginFailureAction,
+  loginSuccessAction,
+} from 'src/app/auth/store/actions/login.action'
+import { catchError, of, switchMap, map, tap } from 'rxjs'
+import { AuthService } from 'src/app/auth/services/auth.service'
+import { CurrentUserInterface } from 'src/app/shared/types/interfaces/currentUser.interface'
+import { Router } from '@angular/router'
+import { HttpErrorResponse } from '@angular/common/http'
+import { CreateArticleService } from 'src/app/createArticle/services/createArticle.service'
+
+@Injectable()
+export class LoginEffect {
+  private login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loginAction),
+      switchMap(({ request }) => {
+        return this.authService.login(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return loginSuccessAction({ currentUser })
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              loginFailureAction({ errors: errorResponse.error.errors }),
+            )
+          }),
+        )
+      }),
+    ),
+  )
+
+  private redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/')
+        }),
+      ),
+    { dispatch: false },
+  )
+
+  constructor(
+    private actions$: Actions,
+    private createArticleService: CreateArticleService,
+    private router: Router,
+  ) {}
+}
