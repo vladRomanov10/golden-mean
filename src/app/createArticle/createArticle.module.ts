02ed5795dd4f07d980f6eb9390587045a7e76225
Vlad Romanov <vladromeur@gmail.com>
2025-06-25 17:41:52 +0500

    Запровайдил CreateArticleService

--- src/app/createArticle/createArticle.module.ts
+++ src/app/createArticle/createArticle.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common'
 import { CreateArticleComponent } from 'src/app/createArticle/components/createArticle/createArticle.component'
 import { RouterModule } from '@angular/router'
 import { ArticleFormModule } from 'src/app/shared/modules/articleForm/articleForm.module'
+import { CreateArticleService } from 'src/app/createArticle/services/createArticle.service'
 
 const routes = [{ path: 'articles/new', component: CreateArticleComponent }]
 
 @NgModule({
   declarations: [CreateArticleComponent],
   imports: [CommonModule, RouterModule.forChild(routes), ArticleFormModule],
+  providers: [CreateArticleService],
 })
 export class CreateArticleModule {}
