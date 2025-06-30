de7055374731f1145309dbb43d30e76907213208
Vlad Romanov <vladromeur@gmail.com>
2025-06-30 21:24:16 +0500

    Добавил модуль эффектов и модуль редьюсеров

--- src/app/createArticle/createArticle.module.ts
+++ src/app/createArticle/createArticle.module.ts
@@ -4,12 +4,22 @@ import { CreateArticleComponent } from 'src/app/createArticle/components/createA
 import { RouterModule } from '@angular/router'
 import { ArticleFormModule } from 'src/app/shared/modules/articleForm/articleForm.module'
 import { CreateArticleService } from 'src/app/createArticle/services/createArticle.service'
+import { EffectsModule } from '@ngrx/effects'
+import { CreateArticleEffect } from 'src/app/createArticle/store/effects/createArticle.effect'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from 'src/app/createArticle/store/reducers'
 
 const routes = [{ path: 'articles/new', component: CreateArticleComponent }]
 
 @NgModule({
   declarations: [CreateArticleComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), ArticleFormModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ArticleFormModule,
+    EffectsModule.forFeature([CreateArticleEffect]),
+    StoreModule.forFeature('createArticle', reducers),
+  ],
   providers: [CreateArticleService],
 })
 export class CreateArticleModule {}
