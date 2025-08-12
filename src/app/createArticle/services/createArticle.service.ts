84036f2ab406bc2b36ac74fe725f33aec0c268e0
Vlad Romanov <vladromeur@gmail.com>
2025-08-12 15:32:56 +0500

    rename интерфейса

--- src/app/createArticle/services/createArticle.service.ts
+++ src/app/createArticle/services/createArticle.service.ts
@@ -5,14 +5,14 @@ import { map, Observable } from 'rxjs'
 import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 import { environment } from 'src/environments/environment'
 import { PostArticleResponseInterface } from 'src/app/shared/types/aliases/articleResponse.aliases'
-import { CreateArticleRequest } from 'src/app/shared/types/aliases/articleRequest.aliases'
+import { PostArticleRequestInterface } from 'src/app/shared/types/aliases/articleRequest.aliases'
 
 @Injectable()
 export class CreateArticleService {
   constructor(private http: HttpClient) {}
 
   createArticle(
-    articleInput: CreateArticleRequest,
+    articleInput: PostArticleRequestInterface,
   ): Observable<ArticleInterface> {
     const fullUrl = environment.apiUrl + '/articles'
 
