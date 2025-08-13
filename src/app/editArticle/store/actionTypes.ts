0791e4699a0c12b721822087c473d0b970eb4e6a
Vlad Romanov <vladromeur@gmail.com>
2025-08-13 17:42:51 +0500

    Прописал actionTypes

--- src/app/editArticle/store/actionTypes.ts
+++ src/app/editArticle/store/actionTypes.ts
@@ -1,5 +1,9 @@
 export enum ActionTypes {
-  CREATE_ARTICLE = '[Create Article] Create Article',
-  CREATE_ARTICLE_SUCCESS = '[Create Article] Create Article success',
-  CREATE_ARTICLE_FAILURE = '[Create Article] Create Article failure',
+  UPDATE_ARTICLE = '[Edit Article] Update Article',
+  UPDATE_ARTICLE_SUCCESS = '[Edit Article] Update Article success',
+  UPDATE_ARTICLE_FAILURE = '[Edit Article] Update Article failure',
+
+  GET_ARTICLE = '[Edit Article] Get Article',
+  GET_ARTICLE_SUCCESS = '[Edit Article] Get Article success',
+  GET_ARTICLE_FAILURE = '[Edit Article] Get Article failure',
 }
