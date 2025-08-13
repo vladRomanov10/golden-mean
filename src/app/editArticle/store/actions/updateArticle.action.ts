e38b2d710cad26c3e45381f10d087c9190555184
Vlad Romanov <vladromeur@gmail.com>
2025-08-13 17:43:36 +0500

    Прописал экшены

--- src/app/editArticle/store/actions/updateArticle.action.ts
+++ src/app/editArticle/store/actions/updateArticle.action.ts
@@ -1,20 +1,21 @@
 import { createAction, props } from '@ngrx/store'
-import { ActionTypes } from 'src/app/createArticle/store/actionTypes'
+
 import { ArticleInputInterface } from 'src/app/shared/types/interfaces/articleInput.interface'
 import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 import { BackendErrorsInterface } from 'src/app/shared/types/interfaces/backendErrors.interface'
+import { ActionTypes } from 'src/app/editArticle/store/actionTypes'
 
-export const createArticleAction = createAction(
-  ActionTypes.CREATE_ARTICLE,
-  props<{ articleInput: ArticleInputInterface }>(),
+export const updateArticleAction = createAction(
+  ActionTypes.UPDATE_ARTICLE,
+  props<{ slug: string; articleInput: ArticleInputInterface }>(),
 )
 
-export const createArticleSuccessAction = createAction(
-  ActionTypes.CREATE_ARTICLE_SUCCESS,
+export const updateArticleSuccessAction = createAction(
+  ActionTypes.UPDATE_ARTICLE_SUCCESS,
   props<{ article: ArticleInterface }>(),
 )
 
-export const createArticleFailureAction = createAction(
-  ActionTypes.CREATE_ARTICLE_FAILURE,
+export const updateArticleFailureAction = createAction(
+  ActionTypes.UPDATE_ARTICLE_FAILURE,
   props<{ errors: BackendErrorsInterface }>(),
 )
