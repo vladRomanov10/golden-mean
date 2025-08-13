aa8d27ec61e5b0258d3a7b349c7a567f444da77b
Vlad Romanov <vladromeur@gmail.com>
2025-08-13 15:51:20 +0500

    Добавил интерфейс для Put

--- src/app/shared/types/aliases/articleRequest.aliases.ts
+++ src/app/shared/types/aliases/articleRequest.aliases.ts
@@ -1,3 +1,4 @@
 import { ArticleRequestInterface } from 'src/app/shared/types/interfaces/articleRequest.interface'
 
 export type PostArticleRequestInterface = ArticleRequestInterface
+export type PutArticleRequestInterface = ArticleRequestInterface
