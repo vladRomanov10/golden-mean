77c2981f2aee210302993f61e096ae802f2bbb29
Vlad Romanov <vladromeur@gmail.com>
2025-08-14 15:15:37 +0500

    Исправил значения поля

--- src/app/editArticle/types/editArticleState.interface.ts
+++ src/app/editArticle/types/editArticleState.interface.ts
@@ -2,7 +2,7 @@ import { BackendErrorsInterface } from 'src/app/shared/types/interfaces/backendE
 import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 
 export interface EditArticleStateInterface {
-  isLoading: true
+  isLoading: boolean
   article: ArticleInterface | null
   isSubmitting: boolean
   validationErrors: BackendErrorsInterface | null
