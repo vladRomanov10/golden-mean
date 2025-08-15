32ec32f588e238b431d5e4f438735050077b6986
Vlad Romanov <vladromeur@gmail.com>
2025-08-15 15:13:06 +0500

    Добавил loadingModule

--- src/app/editArticle/editArticle.module.ts
+++ src/app/editArticle/editArticle.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { CreateArticleComponent } from 'src/app/createArticle/components/createArticle/createArticle.component'
 import { RouterModule } from '@angular/router'
 import { ArticleFormModule } from 'src/app/shared/modules/articleForm/articleForm.module'
 import { EffectsModule } from '@ngrx/effects'
@@ -10,17 +9,22 @@ import { ArticleService as SharedArticleService } from 'src/app/shared/services/
 import { UpdateArticleEffect } from 'src/app/editArticle/store/effects/updateArticle.effect'
 import { GetArticleEffect } from 'src/app/editArticle/store/effects/getArticle.effect'
 import { reducers } from 'src/app/editArticle/store/reducers'
+import { EditArticleComponent } from 'src/app/editArticle/components/editArticle/editArticle.component'
+import { LoadingModule } from 'src/app/shared/modules/loading/loading.module'
 
-const routes = [{ path: 'articles/new', component: CreateArticleComponent }]
+const routes = [
+  { path: 'articles/:slug/edit', component: EditArticleComponent },
+]
 
 @NgModule({
-  declarations: [CreateArticleComponent],
+  declarations: [EditArticleComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     ArticleFormModule,
     EffectsModule.forFeature([UpdateArticleEffect, GetArticleEffect]),
     StoreModule.forFeature('editArticle', reducers),
+    LoadingModule,
   ],
   providers: [EditArticleService, SharedArticleService],
 })
