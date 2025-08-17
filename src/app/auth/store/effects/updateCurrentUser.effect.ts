a08d6759c974e24f1a42568355c43ca5e1891e41
Vlad Romanov <vladromeur@gmail.com>
2025-08-17 18:16:21 +0500

    Создал эффект

---
+++ src/app/auth/store/effects/updateCurrentUser.effect.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { catchError, of, switchMap, map } from 'rxjs'
+import { AuthService } from 'src/app/auth/services/auth.service'
+import { CurrentUserInterface } from 'src/app/shared/types/interfaces/currentUser.interface'
+import { HttpErrorResponse } from '@angular/common/http'
+import {
+  updateCurrentUserAction,
+  updateCurrentUserFailureAction,
+  updateCurrentUserSuccessAction,
+} from 'src/app/auth/store/actions/updateCurrentUser.action'
+
+@Injectable()
+export class UpdateCurrentUserEffect {
+  private updateCurrentUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(updateCurrentUserAction),
+      switchMap(({ currentUserInput }) => {
+        return this.authService.updateCurrentUser(currentUserInput).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return updateCurrentUserSuccessAction({ currentUser })
+          }),
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              updateCurrentUserFailureAction({
+                errors: errorResponse.error.errors,
+              }),
+            )
+          }),
+        )
+      }),
+    ),
+  )
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+  ) {}
+}
