db3515eb88a64ea153a511c5f5292304275fece7
Vlad Romanov <vladromeur@gmail.com>
2025-08-17 18:50:56 +0500

    Добавил settingsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { TagFeedModule } from 'src/app/tagFeed/tagFeed.module'
 import { ArticleModule } from 'src/app/article/article.module'
 import { CreateArticleModule } from 'src/app/createArticle/createArticle.module'
 import { EditArticleModule } from 'src/app/editArticle/editArticle.module'
+import { SettingsModule } from 'src/app/settings/settings.module'
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,6 +43,7 @@ import { EditArticleModule } from 'src/app/editArticle/editArticle.module'
     CreateArticleModule,
     EditArticleModule,
     ArticleModule,
+    SettingsModule,
     StoreRouterConnectingModule.forRoot(),
   ],
   providers: [
