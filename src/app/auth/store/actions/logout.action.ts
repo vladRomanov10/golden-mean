c628add2aa83c9771ff8b4309b4cb927b2251f2b
Vlad Romanov <vladromeur@gmail.com>
2025-08-20 14:39:09 +0500

    Bug fix, заменил на правильные экшен

--- src/app/auth/store/actions/logout.action.ts
+++ src/app/auth/store/actions/logout.action.ts
@@ -3,6 +3,6 @@ import { ActionTypes } from 'src/app/auth/store/actionTypes'
 
 export const logoutAction = createAction(ActionTypes.LOGOUT)
 
-export const logoutSuccessAction = createAction(ActionTypes.LOGIN_SUCCESS)
+export const logoutSuccessAction = createAction(ActionTypes.LOGOUT_SUCCESS)
 
-export const logoutFailureAction = createAction(ActionTypes.LOGIN_FAILURE)
+export const logoutFailureAction = createAction(ActionTypes.LOGOUT_FAILURE)
