0e71058054e90969fb2936d1ca463ee37aab4442
Vlad Romanov <vladromeur@gmail.com>
2025-08-25 20:11:18 +0500

    Создал сервис и прописал метод для добавления в избранное

---
+++ src/app/shared/modules/favorite/services/favorite.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { map, Observable } from 'rxjs'
+import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
+import { environment } from 'src/environments/environment'
+import { ArticleResponseInterface } from 'src/app/shared/types/interfaces/articleResponse.interface'
+
+@Injectable()
+export class FavoriteService {
+  constructor(private http: HttpClient) {}
+
+  addToFavorites(slug: string): Observable<ArticleInterface> {
+    const url = `${environment.apiUrl}/articles/${slug}/favorite`
+
+    return this.http.post<ArticleResponseInterface>(url, null).pipe(
+      map((response: ArticleResponseInterface) => {
+        return response.article
+      }),
+    )
+  }
+}
