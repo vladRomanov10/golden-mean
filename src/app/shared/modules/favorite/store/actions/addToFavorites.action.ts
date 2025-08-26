86e2fa95819908e39e15c454baa9a91733768493
Vlad Romanov <vladromeur@gmail.com>
2025-08-26 17:21:16 +0500

    Добавил пропсы в экшены

--- src/app/shared/modules/favorite/store/actions/addToFavorites.action.ts
+++ src/app/shared/modules/favorite/store/actions/addToFavorites.action.ts
@@ -1,9 +1,14 @@
-import { createAction } from '@ngrx/store'
+import { createAction, props } from '@ngrx/store'
 import { ActionTypes } from 'src/app/shared/modules/favorite/store/actionTypes'
+import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 
-export const addToFavoritesAction = createAction(ActionTypes.ADD_TO_FAVORITE)
+export const addToFavoritesAction = createAction(
+  ActionTypes.ADD_TO_FAVORITE,
+  props<{ slug: string }>(),
+)
 export const addToFavoritesSuccessAction = createAction(
   ActionTypes.ADD_TO_FAVORITE_SUCCESS,
+  props<{ article: ArticleInterface }>(),
 )
 export const addToFavoritesFailureAction = createAction(
   ActionTypes.ADD_TO_FAVORITE_FAILURE,
