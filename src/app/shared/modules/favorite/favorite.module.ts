896b72469ee383c598aa0e9a2ddd4534b5a793d2
Vlad Romanov <vladromeur@gmail.com>
2025-08-26 17:50:45 +0500

    Добавил модуль эффектов и запровайдил сервис

--- src/app/shared/modules/favorite/favorite.module.ts
+++ src/app/shared/modules/favorite/favorite.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FavoriteComponent } from './components/favorite/favorite.component';
-
-
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FavoriteComponent } from './components/favorite/favorite.component'
+import { EffectsModule } from '@ngrx/effects'
+import { AddToFavoritesEffect } from 'src/app/shared/modules/favorite/store/effects/addToFavorites.effect'
+import { FavoriteService } from 'src/app/shared/modules/favorite/services/favorite.service'
 
 @NgModule({
   declarations: [FavoriteComponent],
   exports: [FavoriteComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([AddToFavoritesEffect])],
+  providers: [FavoriteService],
 })
 export class FavoriteModule {}
