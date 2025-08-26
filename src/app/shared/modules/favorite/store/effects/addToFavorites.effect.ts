32d6984e2c1a3213188e52a4481d9af2753a6ff0
Vlad Romanov <vladromeur@gmail.com>
2025-08-26 17:19:04 +0500

    Добавил эффект

---
+++ src/app/shared/modules/favorite/store/effects/addToFavorites.effect.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import {
+  addToFavoritesAction,
+  addToFavoritesFailureAction,
+  addToFavoritesSuccessAction,
+} from 'src/app/shared/modules/favorite/store/actions/addToFavorites.action'
+import { catchError, map, of, switchMap } from 'rxjs'
+import { FavoriteService } from 'src/app/shared/modules/favorite/services/favorite.service'
+import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
+
+@Injectable()
+export class AddToFavoritesEffect {
+  private addToFavorites$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(addToFavoritesAction),
+      switchMap(({ slug }) => {
+        return this.favoriteService.addToFavorites(slug).pipe(
+          map((article: ArticleInterface) => {
+            return addToFavoritesSuccessAction({ article })
+          }),
+          catchError(() => {
+            return of(addToFavoritesFailureAction())
+          }),
+        )
+      }),
+    ),
+  )
+
+  constructor(
+    private actions$: Actions,
+    private favoriteService: FavoriteService,
+  ) {}
+}
