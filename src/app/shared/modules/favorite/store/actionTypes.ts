c193688191e64b8a4f79636ede724968f6abd318
Vlad Romanov <vladromeur@gmail.com>
2025-08-26 15:26:11 +0500

    Создал экшн типы

---
+++ src/app/shared/modules/favorite/store/actionTypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  ADD_TO_FAVORITE = '[Favorite] Add to favorite',
+  ADD_TO_FAVORITE_SUCCESS = '[Favorite] Add to favorite success',
+  ADD_TO_FAVORITE_FAILURE = '[Favorite] Add to favorite failure',
+}
