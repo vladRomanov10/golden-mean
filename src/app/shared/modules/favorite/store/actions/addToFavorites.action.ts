a1025ba93337725507b4710126a025f9f0994c27
Vlad Romanov <vladromeur@gmail.com>
2025-08-27 17:54:20 +0500

    Исправил опечатку

--- src/app/shared/modules/favorite/store/actions/addToFavorites.action.ts
+++ src/app/shared/modules/favorite/store/actions/addToFavorites.action.ts
@@ -3,13 +3,13 @@ import { ActionTypes } from 'src/app/shared/modules/favorite/store/actionTypes'
 import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 
 export const addToFavoritesAction = createAction(
-  ActionTypes.ADD_TO_FAVORITE,
+  ActionTypes.ADD_TO_FAVORITES,
   props<{ slug: string }>(),
 )
 export const addToFavoritesSuccessAction = createAction(
-  ActionTypes.ADD_TO_FAVORITE_SUCCESS,
+  ActionTypes.ADD_TO_FAVORITES_SUCCESS,
   props<{ article: ArticleInterface }>(),
 )
 export const addToFavoritesFailureAction = createAction(
-  ActionTypes.ADD_TO_FAVORITE_FAILURE,
+  ActionTypes.ADD_TO_FAVORITES_FAILURE,
 )
