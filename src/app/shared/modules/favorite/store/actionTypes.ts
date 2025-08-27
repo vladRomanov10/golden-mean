91a8b1df39a029a23c2c34098b0424d033b7b087
Vlad Romanov <vladromeur@gmail.com>
2025-08-27 17:53:21 +0500

    Добавил экшен тайпов для удаления

--- src/app/shared/modules/favorite/store/actionTypes.ts
+++ src/app/shared/modules/favorite/store/actionTypes.ts
@@ -1,5 +1,9 @@
 export enum ActionTypes {
-  ADD_TO_FAVORITE = '[Favorite] Add to favorite',
-  ADD_TO_FAVORITE_SUCCESS = '[Favorite] Add to favorite success',
-  ADD_TO_FAVORITE_FAILURE = '[Favorite] Add to favorite failure',
+  ADD_TO_FAVORITES = '[Favorite] Add to favorite',
+  ADD_TO_FAVORITES_SUCCESS = '[Favorite] Add to favorite success',
+  ADD_TO_FAVORITES_FAILURE = '[Favorite] Add to favorite failure',
+
+  DELETE_FROM_FAVORITES = '[Favorite] Delete from favorite',
+  DELETE_FROM_FAVORITES_SUCCESS = '[Favorite] Delete from favorite success',
+  DELETE_FROM_FAVORITES_FAILURE = '[Favorite] Delete from favorite failure',
 }
