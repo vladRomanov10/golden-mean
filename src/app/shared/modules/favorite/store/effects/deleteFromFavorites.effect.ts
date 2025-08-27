8f6e40e0e58d3d9695df66fc862ac78695bf2c39
Vlad Romanov <vladromeur@gmail.com>
2025-08-27 18:33:04 +0500

    Написал эффект

---
+++ src/app/shared/modules/favorite/store/effects/deleteFromFavorites.effect.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { FavoriteService } from 'src/app/shared/modules/favorite/services/favorite.service'
+import {
+  deleteFromFavoritesAction,
+  deleteFromFavoritesFailureAction,
+  deleteFromFavoritesSuccessAction,
+} from 'src/app/shared/modules/favorite/store/actions/deleteFromFavorites.action'
+import { catchError, map, of, switchMap } from 'rxjs'
+import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
+
+@Injectable()
+export class DeleteFromFavoritesEffect {
+  private deleteFromFavorites$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(deleteFromFavoritesAction),
+      switchMap(({ slug }) => {
+        return this.favouriteService.deleteFromFavorites(slug).pipe(
+          map((article: ArticleInterface) => {
+            return deleteFromFavoritesSuccessAction({ article })
+          }),
+          catchError(() => {
+            return of(deleteFromFavoritesFailureAction())
+          }),
+        )
+      }),
+    ),
+  )
+
+  constructor(
+    private actions$: Actions,
+    private favouriteService: FavoriteService,
+  ) {}
+}
