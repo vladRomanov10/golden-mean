44c7e5e4d8495daaa876b48528577ff76bc1891f
Vlad Romanov <vladromeur@gmail.com>
2025-08-28 18:05:58 +0500

    Добавил фишку optimistic updates

--- src/app/shared/modules/favorite/components/favorite/favorite.component.ts
+++ src/app/shared/modules/favorite/components/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { addToFavoritesAction } from 'src/app/shared/modules/favorite/store/actions/addToFavorites.action'
 import { deleteFromFavoritesAction } from 'src/app/shared/modules/favorite/store/actions/deleteFromFavorites.action'
@@ -7,19 +7,30 @@ import { deleteFromFavoritesAction } from 'src/app/shared/modules/favorite/store
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
 })
-export class FavoriteComponent {
+export class FavoriteComponent implements OnInit {
   @Input('favoritesCount') favoritesCountProps!: number
   @Input('articleSlug') articleSlugProps!: string
-  @Input('favorited') favoritedProps!: boolean
+  @Input('isFavorited') isFavoritedProps!: boolean
+
+  public favoritesCount!: number
+  public isFavorited!: boolean
 
   constructor(private store: Store) {}
 
-  toggleFavorite(): void {
-    if (this.favoritedProps) {
+  ngOnInit(): void {
+    this.favoritesCount = this.favoritesCountProps
+    this.isFavorited = this.isFavoritedProps
+  }
+
+  handleLike(): void {
+    if (this.isFavorited) {
+      this.favoritesCount = this.favoritesCount - 1
       this.deleteFromFavorites(this.articleSlugProps)
     } else {
+      this.favoritesCount = this.favoritesCount + 1
       this.addToFavorites(this.articleSlugProps)
     }
+    this.isFavorited = !this.isFavorited
   }
 
   private addToFavorites(slug: string): void {
