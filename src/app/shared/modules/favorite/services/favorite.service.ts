432b4a3b14e5e60cfbb9c36a6772724cf137d57e
Vlad Romanov <vladromeur@gmail.com>
2025-08-29 15:47:11 +0500

    Добавил новые приватные методы и изменил типы получаемые с бэка

--- src/app/shared/modules/favorite/services/favorite.service.ts
+++ src/app/shared/modules/favorite/services/favorite.service.ts
@@ -3,29 +3,38 @@ import { HttpClient } from '@angular/common/http'
 import { map, Observable } from 'rxjs'
 import { ArticleInterface } from 'src/app/shared/types/interfaces/article.interface'
 import { environment } from 'src/environments/environment'
-import { ArticleResponseInterface } from 'src/app/shared/types/interfaces/articleResponse.interface'
+import {
+  AddToFavoritesResponseInterface,
+  RemoveFromFavoritesResponseInterface,
+} from 'src/app/shared/types/aliases/articleResponse.aliases'
 
 @Injectable()
 export class FavoriteService {
   constructor(private http: HttpClient) {}
 
   addToFavorites(slug: string): Observable<ArticleInterface> {
-    const url = `${environment.apiUrl}/articles/${slug}/favorite`
-
-    return this.http.post<ArticleResponseInterface>(url, null).pipe(
-      map((response: ArticleResponseInterface) => {
-        return response.article
-      }),
-    )
+    const url = this.getUrl(slug)
+    return this.http
+      .post<AddToFavoritesResponseInterface>(url, null)
+      .pipe(map(this.getArticle))
   }
 
-  deleteFromFavorites(slug: string): Observable<ArticleInterface> {
-    const url = `${environment.apiUrl}/articles/${slug}/favorite`
+  removeFromFavorites(slug: string): Observable<ArticleInterface> {
+    const url = this.getUrl(slug)
+    return this.http
+      .delete<RemoveFromFavoritesResponseInterface>(url)
+      .pipe(map(this.getArticle))
+  }
 
-    return this.http.delete<ArticleResponseInterface>(url).pipe(
-      map((response: ArticleResponseInterface) => {
-        return response.article
-      }),
-    )
+  private getUrl(slug: string): string {
+    return `${environment.apiUrl}/articles/${slug}/favorite`
+  }
+
+  private getArticle(
+    response:
+      | AddToFavoritesResponseInterface
+      | RemoveFromFavoritesResponseInterface,
+  ): ArticleInterface {
+    return response.article
   }
 }
