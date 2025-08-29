b285a5adf83a4716ad162a7df97878f51d6a76a0
Vlad Romanov <vladromeur@gmail.com>
2025-08-29 18:33:43 +0500

    Добавил новые методы для начальной инициализации, добавил рероут если пользователь не залогинен, но лайкает пост

--- src/app/shared/modules/favorite/components/favorite/favorite.component.ts
+++ src/app/shared/modules/favorite/components/favorite/favorite.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { select, Store } from '@ngrx/store'
 import { addToFavoritesAction } from 'src/app/shared/modules/favorite/store/actions/addToFavorites.action'
-import { removeFromFavoritesAction } from 'src/app/shared/modules/favorite/store/actions/removeFromFavoritesAction'
+import { removeFromFavoritesAction } from 'src/app/shared/modules/favorite/store/actions/removeFromFavorites.action'
+import { Subscription } from 'rxjs'
+import { isLoggedInSelector } from 'src/app/auth/store/selectors'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent implements OnInit, OnDestroy {
   @Input('favoritesCount') favoritesCountProps!: number
   @Input('articleSlug') articleSlugProps!: string
   @Input('isFavorited') isFavoritedProps!: boolean
@@ -15,14 +18,29 @@ export class FavoriteComponent implements OnInit {
   public favoritesCount!: number
   public isFavorited!: boolean
 
-  constructor(private store: Store) {}
+  private isLoggedIn!: boolean | null
+  private isLoggedInSubscription!: Subscription
+
+  constructor(
+    private store: Store,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
-    this.favoritesCount = this.favoritesCountProps
-    this.isFavorited = this.isFavoritedProps
+    this.initializeValues()
+    this.initializeListeners()
+  }
+
+  ngOnDestroy() {
+    this.isLoggedInSubscription.unsubscribe()
   }
 
   handleLike(): void {
+    if (!this.isLoggedIn) {
+      this.router.navigateByUrl('/login')
+      return
+    }
+
     if (this.isFavorited) {
       this.removeFromFavorites(this.articleSlugProps)
     } else {
@@ -39,5 +57,18 @@ export class FavoriteComponent implements OnInit {
   private removeFromFavorites(slug: string): void {
     this.favoritesCount = this.favoritesCount - 1
     this.store.dispatch(removeFromFavoritesAction({ slug }))
+  }
+
+  private initializeValues(): void {
+    this.favoritesCount = this.favoritesCountProps
+    this.isFavorited = this.isFavoritedProps
+  }
+
+  private initializeListeners(): void {
+    this.isLoggedInSubscription = this.store
+      .pipe(select(isLoggedInSelector))
+      .subscribe((isLoggedInSelector) => {
+        this.isLoggedIn = isLoggedInSelector
+      })
   }
 }
