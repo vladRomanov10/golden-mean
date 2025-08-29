f304d89d0e8ab8aa9deb0fae292cdf32f1d63cbb
Vlad Romanov <vladromeur@gmail.com>
2025-08-29 18:34:54 +0500

    rename

--- src/app/shared/modules/favorite/favorite.module.ts
+++ src/app/shared/modules/favorite/favorite.module.ts
@@ -4,14 +4,14 @@ import { FavoriteComponent } from './components/favorite/favorite.component'
 import { EffectsModule } from '@ngrx/effects'
 import { AddToFavoritesEffect } from 'src/app/shared/modules/favorite/store/effects/addToFavorites.effect'
 import { FavoriteService } from 'src/app/shared/modules/favorite/services/favorite.service'
-import { DeleteFromFavoritesEffect } from 'src/app/shared/modules/favorite/store/effects/deleteFromFavorites.effect'
+import { RemoveFromFavoritesEffect } from 'src/app/shared/modules/favorite/store/effects/removeFromFavorites.effect'
 
 @NgModule({
   declarations: [FavoriteComponent],
   exports: [FavoriteComponent],
   imports: [
     CommonModule,
-    EffectsModule.forFeature([AddToFavoritesEffect, DeleteFromFavoritesEffect]),
+    EffectsModule.forFeature([AddToFavoritesEffect, RemoveFromFavoritesEffect]),
   ],
   providers: [FavoriteService],
 })
