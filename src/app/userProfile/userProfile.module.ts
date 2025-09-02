d6250a8204c6a119f90a5a720cc86a23e3b7aad5
Vlad Romanov <vladromeur@gmail.com>
2025-09-02 14:44:32 +0500

    Создал файлы под компонент

---
+++ src/app/userProfile/userProfile.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { UserProfileComponent } from './components/userProfile/userProfile.component'
+
+@NgModule({
+  declarations: [UserProfileComponent],
+  imports: [CommonModule],
+})
+export class UserProfileModule {}
