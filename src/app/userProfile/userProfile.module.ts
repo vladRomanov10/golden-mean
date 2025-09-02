5762c5763df14c57b552a56c4feb2318c33b1bf4
Vlad Romanov <vladromeur@gmail.com>
2025-09-02 22:44:05 +0500

    Запровайдел сервис

--- src/app/userProfile/userProfile.module.ts
+++ src/app/userProfile/userProfile.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { UserProfileComponent } from './components/userProfile/userProfile.component'
 import { RouterModule } from '@angular/router'
+import { UserProfileService } from 'src/app/userProfile/services/userProfile.service'
 
 const routes = [
   { path: 'profiles/:slug', component: UserProfileComponent },
@@ -11,5 +12,6 @@ const routes = [
 @NgModule({
   declarations: [UserProfileComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
+  providers: [UserProfileService],
 })
 export class UserProfileModule {}
