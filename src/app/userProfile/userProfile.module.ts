f980b9dda30e5952abe0df6e131cff9341feeede
Vlad Romanov <vladromeur@gmail.com>
2025-09-02 15:28:24 +0500

    Добавил рауты

--- src/app/userProfile/userProfile.module.ts
+++ src/app/userProfile/userProfile.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { UserProfileComponent } from './components/userProfile/userProfile.component'
+import { RouterModule } from '@angular/router'
+
+const routes = [
+  { path: 'profiles/:slug', component: UserProfileComponent },
+  { path: 'profiles/:slug/favorites', component: UserProfileComponent },
+]
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class UserProfileModule {}
