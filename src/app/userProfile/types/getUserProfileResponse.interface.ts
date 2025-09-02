c75e9062ab25cf05ad6492d1545779c34fc588a7
Vlad Romanov <vladromeur@gmail.com>
2025-09-02 17:34:32 +0500

    Создал интерфейс

---
+++ src/app/userProfile/types/getUserProfileResponse.interface.ts
@@ -0,0 +1,5 @@
+import { UserProfileInterface } from 'src/app/shared/types/interfaces/userProfile.interface'
+
+export interface GetUserProfileResponseInterface {
+  profile: UserProfileInterface
+}
