01bd297536e55a2643121e933f22300795d90a0c
Vlad Romanov <vladromeur@gmail.com>
2025-09-03 15:08:42 +0500

    Дописал 2 экшена

--- src/app/userProfile/store/actions/getUserProfile.action.ts
+++ src/app/userProfile/store/actions/getUserProfile.action.ts
@@ -1,7 +1,17 @@
 import { createAction, props } from '@ngrx/store'
 import { ActionTypes } from 'src/app/userProfile/store/actionTypes'
+import { UserProfileInterface } from 'src/app/shared/types/interfaces/userProfile.interface'
 
 export const getUserProfileAction = createAction(
   ActionTypes.GET_USER_PROFILE,
   props<{ slug: string }>(),
+)
+
+export const getUserProfileSuccessAction = createAction(
+  ActionTypes.GET_USER_PROFILE_SUCCESS,
+  props<{ userProfile: UserProfileInterface }>(),
+)
+
+export const getUserProfileFailureAction = createAction(
+  ActionTypes.GET_USER_PROFILE_FAILURE,
 )
