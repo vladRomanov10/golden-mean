ec6d4510a759f34cd42d54fe0e85f5ab8e9d65f0
Vlad Romanov <vladromeur@gmail.com>
2025-09-04 15:14:42 +0500

    Удалил не используемые псевдонимы

---
+++ src/app/shared/modules/addToFavorites/types/aliases/addToFavoritesResponse.aliases.ts
@@ -0,0 +1,4 @@
+import { ArticleResponseInterface } from 'src/app/shared/types/interfaces/articleResponse.interface'
+
+export type AddToFavoritesResponseInterface = ArticleResponseInterface
+export type RemoveFromFavoritesResponseInterface = ArticleResponseInterface
