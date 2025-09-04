9b94b7921d9f0fc1731d2fb79413d877c1df4896
Vlad Romanov <vladromeur@gmail.com>
2025-09-04 15:15:48 +0500

    Удалил не используемые псевдонимы

--- src/app/shared/types/aliases/articleResponse.aliases.ts
+++ src/app/shared/types/aliases/articleResponse.aliases.ts
@@ -1,8 +1,3 @@
 import { ArticleResponseInterface } from 'src/app/shared/types/interfaces/articleResponse.interface'
 
 export type GetArticleResponseInterface = ArticleResponseInterface
-export type PutArticleResponseInterface = ArticleResponseInterface
-export type PostArticleResponseInterface = ArticleResponseInterface
-
-export type AddToFavoritesResponseInterface = ArticleResponseInterface
-export type RemoveFromFavoritesResponseInterface = ArticleResponseInterface
