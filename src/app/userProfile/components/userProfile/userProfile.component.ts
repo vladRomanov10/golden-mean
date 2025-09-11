7cd75b113ed5aa4ca8b51a266309e27de4784742
Vlad Romanov <vladromeur@gmail.com>
2025-09-11 18:01:45 +0500

    Заполнил компонент userProfile

--- src/app/userProfile/components/userProfile/userProfile.component.ts
+++ src/app/userProfile/components/userProfile/userProfile.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { UserProfileInterface } from 'src/app/shared/types/interfaces/userProfile.interface'
+import { Observable, Subscription } from 'rxjs'
+import { select, Store } from '@ngrx/store'
+import { ActivatedRoute } from '@angular/router'
+import { getUserProfileAction } from 'src/app/userProfile/store/actions/getUserProfile.action'
+import {
+  errorSelector,
+  isLoadingSelector,
+} from 'src/app/userProfile/store/selectors'
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './userProfile.component.html',
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit {
+  public userProfile!: UserProfileInterface
+  public isLoading$!: Observable<boolean>
+  public error$!: Observable<string | null>
+
+  private slug!: string | null
+  private userProfileSubscription!: Subscription
+
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.initializeValues()
+    this.fetchData()
+  }
+
+  private initializeValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug')
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+  }
+
+  private fetchData(): void {
+    this.store.dispatch(getUserProfileAction({ slug: `${this.slug}` }))
+  }
+}
