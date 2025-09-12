9083bacce4caf1026436826f005a472d68256049
Vlad Romanov <vladromeur@gmail.com>
2025-09-12 18:08:55 +0500

    Инициализировал подписчика, добавил onDestroy, добавил инициализацию некоторых переменных

--- src/app/userProfile/components/userProfile/userProfile.component.ts
+++ src/app/userProfile/components/userProfile/userProfile.component.ts
@@ -1,43 +1,78 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { UserProfileInterface } from 'src/app/shared/types/interfaces/userProfile.interface'
-import { Observable, Subscription } from 'rxjs'
+import { combineLatestWith, filter, map, Observable, Subscription } from 'rxjs'
 import { select, Store } from '@ngrx/store'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { getUserProfileAction } from 'src/app/userProfile/store/actions/getUserProfile.action'
 import {
   errorSelector,
   isLoadingSelector,
+  userProfileSelector,
 } from 'src/app/userProfile/store/selectors'
+import { currentUserSelector } from 'src/app/auth/store/selectors'
+import { CurrentUserInterface } from 'src/app/shared/types/interfaces/currentUser.interface'
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './userProfile.component.html',
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, OnDestroy {
   public userProfile!: UserProfileInterface
   public isLoading$!: Observable<boolean>
   public error$!: Observable<string | null>
 
   private slug!: string | null
+  private apiUrl!: string
   private userProfileSubscription!: Subscription
+  private isCurrentUserProfile$!: Observable<boolean>
+  private currentUser$!: Observable<CurrentUserInterface>
 
   constructor(
     private store: Store,
     private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.initializeValues()
     this.fetchData()
+    this.initializeListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.userProfileSubscription.unsubscribe()
   }
 
   private initializeValues(): void {
+    const isFavorites = this.router.url.includes('favorites')
     this.slug = this.route.snapshot.paramMap.get('slug')
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
     this.error$ = this.store.pipe(select(errorSelector))
+    this.currentUser$ = this.store.pipe(
+      select(currentUserSelector),
+      filter(Boolean),
+    )
+    this.apiUrl = isFavorites
+      ? `/articles?favorited=${this.slug}`
+      : `/articles?author=${this.slug}`
+
+    this.isCurrentUserProfile$ = this.currentUser$.pipe(
+      map(
+        (currentUser: CurrentUserInterface) =>
+          this.userProfile.username === currentUser.username,
+      ),
+    )
   }
 
   private fetchData(): void {
     this.store.dispatch(getUserProfileAction({ slug: `${this.slug}` }))
+  }
+
+  private initializeListeners(): void {
+    this.userProfileSubscription = this.store
+      .pipe(select(userProfileSelector), filter(Boolean))
+      .subscribe((userProfile: UserProfileInterface) => {
+        this.userProfile = userProfile
+      })
   }
 }
