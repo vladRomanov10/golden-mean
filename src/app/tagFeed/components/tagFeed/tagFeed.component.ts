95088de1bede1720ce532485d702a4edf5c159cb
Vlad Romanov <vladromeur@gmail.com>
2025-09-15 21:52:56 +0500

    Добавил отписку от стрима

--- src/app/tagFeed/components/tagFeed/tagFeed.component.ts
+++ src/app/tagFeed/components/tagFeed/tagFeed.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-tag-feed',
   templateUrl: './tagFeed.component.html',
   styleUrl: './tagFeed.component.scss',
 })
-export class TagFeedComponent implements OnInit {
+export class TagFeedComponent implements OnInit, OnDestroy {
   public apiUrl!: string
   public tagName!: string | null
+
+  private routeParamsSubscribe!: Subscription
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.tagName = params['slug']
-      this.apiUrl = `/articles?tag=${this.tagName}`
-    })
+    this.routeParamsSubscribe = this.route.params.subscribe(
+      (params: Params) => {
+        this.tagName = params['slug']
+        this.apiUrl = `/articles?tag=${this.tagName}`
+      },
+    )
+  }
+
+  ngOnDestroy() {
+    this.routeParamsSubscribe.unsubscribe()
   }
 }
