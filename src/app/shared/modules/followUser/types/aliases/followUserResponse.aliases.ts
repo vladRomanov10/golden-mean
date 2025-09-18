a2413f5711dfc8953c85ad268a7755ff399dd3a6
Vlad Romanov <vladromeur@gmail.com>
2025-09-18 19:02:08 +0500

    прописал алиасы

---
+++ src/app/shared/modules/followUser/types/aliases/followUserResponse.aliases.ts
@@ -0,0 +1,4 @@
+import { UserProfileResponseInterface } from 'src/app/shared/types/interfaces/userProfileResponse.interface'
+
+export type FollowUserResponseInterface = UserProfileResponseInterface
+export type UnfollowUserResponseInterface = UserProfileResponseInterface
