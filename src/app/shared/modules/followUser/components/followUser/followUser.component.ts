dbee13136884e2e75443d5c1fd3f42cefbffa3bd
Vlad Romanov <vladromeur@gmail.com>
2025-09-18 17:51:52 +0500

    Создал пустышку под компонент

---
+++ src/app/shared/modules/followUser/components/followUser/followUser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-follow-user',
+  templateUrl: './follow-user.component.html',
+  styleUrl: './follow-user.component.scss'
+})
+export class FollowUserComponent {
+
+}
---
+++ src/app/shared/modules/followUser/followUser.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FollowUserComponent } from './components/follow-user/follow-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    FollowUserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FollowButtonModule { }
