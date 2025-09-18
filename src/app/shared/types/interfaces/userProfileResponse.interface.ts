6b3deb6729d4ad98d10e399f0b47ec7f1c04a754
Vlad Romanov <vladromeur@gmail.com>
2025-09-18 18:41:40 +0500

    Сделал приватный интерфейс публичным

--- src/app/shared/types/interfaces/userProfileResponse.interface.ts
+++ src/app/shared/types/interfaces/userProfileResponse.interface.ts
@@ -1,5 +1,5 @@
 import { UserProfileInterface } from 'src/app/shared/types/interfaces/userProfile.interface'
 
-export interface GetUserProfileResponseInterface {
+export interface UserProfileResponseInterface {
   profile: UserProfileInterface
 }
