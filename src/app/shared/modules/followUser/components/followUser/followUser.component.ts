1cb2e716587ed7160f816a854aeaaceaa8c5fc40
Vlad Romanov <vladromeur@gmail.com>
2025-09-19 18:19:39 +0500

    Добавил Инпут в компонент FollowUser

--- src/app/shared/modules/followUser/components/followUser/followUser.component.ts
+++ src/app/shared/modules/followUser/components/followUser/followUser.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-follow-user',
   templateUrl: './followUser.component.html',
 })
-export class FollowUserComponent {}
+export class FollowUserComponent {
+  @Input('isFollowing') isFollowingProps!: boolean
+}
