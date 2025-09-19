dadc64a554a1efd9b6b4bba1951e3eee09dd6504
Vlad Romanov <vladromeur@gmail.com>
2025-09-19 16:34:43 +0500

    Сделал экшен тайпы

---
+++ src/app/shared/modules/followUser/store/actionTypes.ts
@@ -0,0 +1,9 @@
+export enum ActionTypes {
+  FOLLOW_USER = '[Follow user] Follow user',
+  FOLLOW_USER_SUCCESS = '[Follow user] Follow user success',
+  FOLLOW_USER_FAILURE = '[Follow user] Follow user failure',
+
+  UNFOLLOW_USER = '[Follow user] Unfollow user',
+  UNFOLLOW_USER_SUCCESS = '[Follow user] Unfollow user success',
+  UNFOLLOW_USER_FAILURE = '[Follow user] Unfollow user failure',
+}
