a266af87900d1c4ac4486b2b3b20caa0b1cacc0e
Vlad Romanov <vladromeur@gmail.com>
2025-09-19 16:17:48 +0500

    rename action types

--- src/app/shared/modules/addToFavorites/store/actionTypes.ts
+++ src/app/shared/modules/addToFavorites/store/actionTypes.ts
@@ -1,9 +1,9 @@
 export enum ActionTypes {
-  ADD_TO_FAVORITES = '[Favorite] Add to favorite',
-  ADD_TO_FAVORITES_SUCCESS = '[Favorite] Add to favorite success',
-  ADD_TO_FAVORITES_FAILURE = '[Favorite] Add to favorite failure',
+  ADD_TO_FAVORITES = '[Add to favorites] Add to favorite',
+  ADD_TO_FAVORITES_SUCCESS = '[Add to favorites] Add to favorite success',
+  ADD_TO_FAVORITES_FAILURE = '[Add to favorites] Add to favorite failure',
 
-  REMOVE_FROM_FAVORITES = '[Favorite] Remove from favorites',
-  REMOVE_FROM_FAVORITES_SUCCESS = '[Favorite] Remove from favorites success',
-  REMOVE_FROM_FAVORITES_FAILURE = '[Favorite] Remove from favorites failure',
+  REMOVE_FROM_FAVORITES = '[Add to favorites] Remove from favorites',
+  REMOVE_FROM_FAVORITES_SUCCESS = '[Add to favorites] Remove from favorites success',
+  REMOVE_FROM_FAVORITES_FAILURE = '[Add to favorites] Remove from favorites failure',
 }
