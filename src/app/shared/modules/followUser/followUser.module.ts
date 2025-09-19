1a623fee2070c153027eca46c92d86afe58b310d
Vlad Romanov <vladromeur@gmail.com>
2025-09-19 18:17:27 +0500

    Добавил EffectsModule в FollowUserModule

--- src/app/shared/modules/followUser/followUser.module.ts
+++ src/app/shared/modules/followUser/followUser.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FollowUserComponent } from 'src/app/shared/modules/followUser/components/followUser/followUser.component'
+import { EffectsFeatureModule, EffectsModule } from '@ngrx/effects'
+import { FollowUserEffect } from 'src/app/shared/modules/followUser/store/effects/followUser.effect'
+import { UnfollowUserEffect } from 'src/app/shared/modules/followUser/store/effects/unfollowUser.effect'
 
 @NgModule({
   declarations: [FollowUserComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([FollowUserEffect, UnfollowUserEffect]),
+  ],
   exports: [FollowUserComponent],
 })
 export class FollowUserModule {}
