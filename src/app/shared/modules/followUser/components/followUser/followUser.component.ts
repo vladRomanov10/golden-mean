3b9c996f35d481be9f7582d4ec6ae1add1d556c0
Vlad Romanov <vladromeur@gmail.com>
2025-09-21 15:58:49 +0500

    Добавил инпут slug, добавил метод followUser

--- src/app/shared/modules/followUser/components/followUser/followUser.component.ts
+++ src/app/shared/modules/followUser/components/followUser/followUser.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { unfollowUserAction } from 'src/app/shared/modules/followUser/store/actions/unfollowUser.action'
+import { followUserAction } from 'src/app/shared/modules/followUser/store/actions/followUser.action'
 
 @Component({
   selector: 'app-follow-user',
@@ -6,4 +9,15 @@ import { Component, Input } from '@angular/core'
 })
 export class FollowUserComponent {
   @Input('isFollowing') isFollowingProps!: boolean
+  @Input('slug') slugProps!: string | null
+
+  constructor(private store: Store) {}
+
+  followUser(): void {
+    if (this.isFollowingProps) {
+      this.store.dispatch(unfollowUserAction({ slug: `${this.slugProps}` }))
+    } else {
+      this.store.dispatch(followUserAction({ slug: `${this.slugProps}` }))
+    }
+  }
 }
